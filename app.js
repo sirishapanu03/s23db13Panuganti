91e8ee20a224f72e230ee3fd864db5e0c05905b9
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-13 21:14:18 -0500

    added resource and controllers

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cakesRouter = require('./routes/cakes');
 var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
+var Cake = require("./models/cakes");
+var resourceRouter = require('./routes/resource')
 
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +38,7 @@ app.use('/users', usersRouter);
 app.use('/cakes', cakesRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
 
 
 // catch 404 and forward to error handler
@@ -45,5 +56,38 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+// We can seed the collection if needed on server start
+async function recreateDB(){
+ // Delete everything
+ await Cake.deleteMany();
+ let cake1 = new
+Cake({cake_flavour:"Vanilla", weight:'2lb', cake_cost:20});
+ cake1.save().then(doc=>{
+  console.log("First object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+
+ let cake2 = new
+ Cake({cake_flavour:"Vanilla", weight:'2lb', cake_cost:20});
+  cake2.save().then(doc=>{
+    console.log("Second object saved")}
+    ).catch(err=>{
+    console.error(err)
+    });
+
+  let cake3 = new
+  Cake({cake_flavour:"Vanilla", weight:'2lb', cake_cost:20});
+   cake3.save().then(doc=>{
+    console.log("Third object saved")}
+    ).catch(err=>{
+    console.error(err)
+    });
+
+}
+let reseed = true;
+if (reseed) { recreateDB();}
 
 module.exports = app;
---
+++ routes/resource.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+// Require controller modules.
+var api_controller = require('../controllers/api');
+var cake_controller = require('../controllers/cake');
+/// API ROUTE ///
+// GET resources base.
+router.get('/', api_controller.api);
+/// CAKE ROUTES ///
+// POST request for creating a cake.
+router.post('/cakes', cake_controller.cake_create_post);
+// DELETE request to delete cake.
+router.delete('/cakes/:id', cake_controller.cake_delete);
+// PUT request to update cake.
+router.put('/cakes/:id', cake_controller.cake_update_put);
+// GET request for one cake.
+router.get('/cakes/:id', cake_controller.cake_detail);
+// GET request for list of all cake items.
+router.get('/cakes', cake_controller.cake_list);
+module.exports = router;
