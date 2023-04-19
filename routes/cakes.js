73b5dc3871e438d5f3107f0e7e58325ff1be9ec1
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-19 18:49:02 -0500

    moved to cakes.js

--- routes/cakes.js
+++ routes/cakes.js
@@ -1,8 +1,21 @@
 var express = require('express');
 const cake_controlers= require('../controllers/cakes');
+var detail_controller = require('../controllers/detail');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', cake_controlers.cake_view_all_Page);
+
+
+/* GET detail cake page */
+router.get('/detail', detail_controller.cake_view_one_Page);
+/* GET create cake page */
+router.get('/create', detail_controller.cake_create_Page);
+/* GET  update cake page */
+router.get('/update', detail_controller.cake_update_Page);
+
+/* GET delete cake page */
+router.get('/delete', detail_controller.cake_delete_Page);
 
 module.exports = router;
--- routes/resource.js
+++ routes/resource.js
@@ -3,7 +3,6 @@ var router = express.Router();
 // Require controller modules.
 var api_controller = require('../controllers/api');
 var cake_controller = require('../controllers/cakes');
-var detail_controller = require('../controllers/detail')
 /// API ROUTE ///
 // GET resources base.
 router.get('/', api_controller.api);
@@ -20,15 +19,5 @@ router.get('/cakes/:id', cake_controller.cake_detail);
 router.get('/cakes', cake_controller.cake_list);
 router.get('/', cake_controller.cake_view_all_Page);
 
-
-/* GET detail cake page */
-router.get('/cakes/detail', detail_controller.cake_view_one_Page);
-/* GET create cake page */
-router.get('/cakes/create', detail_controller.cake_create_Page);
-/* GET  update cake page */
-router.get('/update', detail_controller.cake_update_Page);
-
-/* GET delete cake page */
-router.get('/delete', detail_controller.cake_delete_Page);
 
 module.exports = router;
