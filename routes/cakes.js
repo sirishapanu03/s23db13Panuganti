4f304d0ad5ef323a29f5672a005e8a79d93a5cf5
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-24 18:00:41 -0500

    bonus pt2

--- routes/cakes.js
+++ routes/cakes.js
@@ -4,6 +4,17 @@ var detail_controller = require('../controllers/detail');
 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+
 /* GET home page. */
 router.get('/', cake_controlers.cake_view_all_Page);
 
@@ -11,23 +22,12 @@ router.get('/', cake_controlers.cake_view_all_Page);
 /* GET detail cake page */
 router.get('/detail', detail_controller.cake_view_one_Page);
 /* GET create cake page */
-router.get('/create', detail_controller.cake_create_Page);
-
-// A little function to check if we have an authorized user and continue on
-//or
-// redirect to login.
-const secured = (req, res, next) => {
- if (req.user){
- return next();
- }
- req.session.returnTo = req.originalUrl;
- res.redirect("/login");
- }
+router.get('/create', secured, detail_controller.cake_create_Page);
 
 /* GET  update cake page */
 router.get('/update',secured, detail_controller.cake_update_Page);
 
 /* GET delete cake page */
-router.get('/delete', detail_controller.cake_delete_Page);
+router.get('/delete',secured, detail_controller.cake_delete_Page);
 
 module.exports = router;
