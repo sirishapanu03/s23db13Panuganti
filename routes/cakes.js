d411fa028668106c2014a62155550c0da0b99266
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-23 19:28:29 -0500

    ss5

--- routes/cakes.js
+++ routes/cakes.js
@@ -12,8 +12,20 @@ router.get('/', cake_controlers.cake_view_all_Page);
 router.get('/detail', detail_controller.cake_view_one_Page);
 /* GET create cake page */
 router.get('/create', detail_controller.cake_create_Page);
+
+// A little function to check if we have an authorized user and continue on
+or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+ 
 /* GET  update cake page */
-router.get('/update', detail_controller.cake_update_Page);
+router.get('/update',secured, detail_controller.cake_update_Page);
 
 /* GET delete cake page */
 router.get('/delete', detail_controller.cake_delete_Page);
--- routes/index.js
+++ routes/index.js
@@ -8,37 +8,37 @@ router.get('/', function (req, res) {
 router.get('/register', function(req, res) {
  res.render('register', { title: 'Cake App Registration'});
 });
-router.post('/register', function(req, res) {
- Account.findOne({ username : req.body.username },
- function(err, user) {
- if(err) {
- return res.render('register', { title: 'Registration',
- message: 'Registration error', account : req.body.username })
- }
- if(user == {} ){
- return res.render('register', { title: 'Registration',
- message: 'Existing User', account : req.body.username })
- }
- let newAccount = new Account({ username : req.body.username });
- Account.register(newAccount, req.body.password, function(err, user){
- if (err) {
- return res.render('register', { title: 'Registration',
- message: 'access error', account : req.body.username })
- }
- if(!user){
- return res.render('register',{ title: 'Registration',
- message: 'access error', account : req.body.username })
- }
- console.log('Sucess, redirect');
- res.redirect('/');
- })
- })
- })
+
+
+ router.post('/register', async function(req, res) {
+  try {
+    const user = await Account.findOne({ username: req.body.username });
+
+    if (user) {
+      return res.render('register', { title: 'Registration', message: 'Existing User', account: req.body.username });
+    }
+
+    let newAccount = new Account({ username: req.body.username });
+
+    const registerUser = await  Account.register(newAccount, req.body.password);
+
+      console.log('Success, redirect');
+      res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    return res.render('register', { title: 'Registration',
+            message: 'Registration error', account : req.body.username });
+  }
+});
+
+ 
 router.get('/login', function(req, res) {
  res.render('login', { title: 'Cake App Login', user : req.user });
 });
 router.post('/login', passport.authenticate('local'), function(req, res) {
- res.redirect('/');
+  if(req.session.returnTo)
+  res.redirect(req.session.returnTo);
+    res.redirect('/');
 });
 router.get('/logout', function(req, res, next) { // use post or delete for better safety
  req.logout( function(err) {
