98e10099a0a292e118a71be4ddcdf39024bd0ed9
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-24 17:39:08 -0500

    added constraints ss6

--- models/cakes.js
+++ models/cakes.js
@@ -1,4 +1,15 @@
 const mongoose = require("mongoose")
-const cakeSchema = mongoose.Schema({ cake_flavour: String, weight: String, cake_cost: Number})
+const cakeSchema = mongoose.Schema({ 
+    cake_flavour: {
+        type:String, required: true, match: /^[a-zA-Z]+$/
+    },
+    weight: {
+        type: String, required: true, match: /^[a-zA-Z0-9]+$/
+    },
+    cake_cost: {
+        type: Number, reqired: true, min:10, max: 1000
+    }
+});
+
 module.exports = mongoose.model("cake",
 cakeSchema)
